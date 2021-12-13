5ffa90018ae43d89a6fc25ce30b911203080b24b
IvanPyzhyanov <piv85@mail.ru>
2021-12-13 14:28:07 +0300

    Realised steps 2: We can see complete post on generated page by number of post

--- app.py
+++ app.py
@@ -11,7 +11,23 @@ data = "data/data.json"
 
 @app.route("/")
 def main_page():
-    return render_template("index.html", data=read_json(data), comments=count_comments(data, data_comments))
+    return render_template("index.html", data=read_json(data), comments_cnt=count_comments(data, data_comments))
+
+
+@app.route("/posts/<int:post_id>")
+def post_page(post_id):
+    if post_id:
+        for post in read_json(data):
+            if post["pk"] == post_id:
+                comments_by_post = [comment for comment in read_json(data_comments) if post_id == comment["post_id"]]
+                return render_template("post.html", post=post, comments_cnt=count_comments(data, data_comments), comments=comments_by_post)
+            # else:
+            #     return "incorrect id", 400
+    else:
+        return "", 400
+
+
+
 
 
 if __name__ == "__main__":
